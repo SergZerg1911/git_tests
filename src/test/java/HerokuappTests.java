5a7ca3eac12bec16ba3911b3dae17a16d7dbedec
sergzerg1911 <sergzerg1911@gmail.com>
2024-12-12 19:34:23 +0300

    added comments to HerokuappTests

--- src/test/java/HerokuappTests.java
+++ src/test/java/HerokuappTests.java
@@ -9,11 +9,17 @@ public class HerokuappTests {
 
     @Test
     void dragAndDropTest() {
+        //Открываем тренировочный сайт
         open("https://the-internet.herokuapp.com/");
+        //Ищем и кликаем по ссылке Drag and Drop
         $(byText("Drag and Drop")).click();
+        //Перетаскиваем элемент A на место элемента B
         $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        //Проверяем что у элемента, на который перетащили первый элемент появился header A
         $("#column-a").$("header").shouldHave(text("B"));
+        //Перетаскиваем элемент обратно
         actions().clickAndHold($("#column-b")).moveToElement($("#column-a")).release().perform();
+        //Проверяем что у элемента A header A, а у элемента B header B
         $("#column-a").$("header").shouldHave(text("A"));
     }
 }
