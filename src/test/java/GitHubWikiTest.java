5506c86535609ab80e6ca111a920987e0acde03f
sergzerg1911 <sergzerg1911@gmail.com>
2024-12-12 21:39:30 +0300

    import optimize

--- src/test/java/GitHubWikiTest.java
+++ src/test/java/GitHubWikiTest.java
@@ -1,10 +1,9 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class GitHubWikiTest {
 
